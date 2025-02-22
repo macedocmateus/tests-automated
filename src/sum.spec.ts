08925202ec03c4b7223333cf891ce10c7293339b
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-02-22 19:49:02 -0300

    add beforeAll for run functions before tests

--- src/sum.spec.ts
+++ src/sum.spec.ts
@@ -2,22 +2,21 @@ import { sum } from "./server";
 
 // Criando um describe para um conjunto de testes
 describe("sum", () => {
+    let sumResult: number;
+
+    // beforeAll ele executa funções antes de ir para os testes
+    beforeAll(() => {
+        sumResult = 10;
+    });
+
     test("sum of 2 + 5 must be 7", () => {
         const result = sum(2, 5);
 
         // Criando uma expectativa, onde ela seja verdadeira, no caso do exemplo resultado = 7
-        expect(result).toBe(7);
+        expect(result).toBe(sumResult);
     });
 
     test("sum of 2 + 2 must be 4", () => {
-        const result = sum(2, 2);
-
-        expect(result).toBe(4);
-    });
-});
-
-describe("sum 2", () => {
-    it("Should do sum of 2 + 2 must be 4", () => {
         const result = sum(2, 2);
 
         expect(result).toBe(4);
