cca441aa6ddf3beabdf92013cd7ce60ec084a4ae
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-02-22 19:11:11 -0300

    add an expectation  and received into sum test using jest

--- src/sum.spec.ts
+++ src/sum.spec.ts
@@ -1,3 +1,8 @@
-test("sum", () => {
-    console.log("test sum ok!");
+import { sum } from "./server";
+
+test("sum of 2 + 5 must be 7", () => {
+    const result = sum(2, 5);
+
+    // Criando uma expectativa, onde ela seja verdadeira, no caso do exemplo resultado = 7
+    expect(result).toBe(7);
 });
