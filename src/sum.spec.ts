55be3b4fece6ead8a6d82fb1a9b36f6dee7215ef
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-02-22 19:58:29 -0300

    add afterAll for run functions after tests

--- src/sum.spec.ts
+++ src/sum.spec.ts
@@ -6,13 +6,19 @@ describe("sum", () => {
 
     // beforeAll ele executa funções antes de ir para os testes
     beforeAll(() => {
-        sumResult = 10;
+        sumResult = 7;
+        console.log("executado antes dos testes", sumResult);
+    });
+
+    // afterAll ele executa funções depois dos testes
+    afterAll(() => {
+        sumResult = 22;
+        console.log("executado depois dos testes", sumResult);
     });
 
     test("sum of 2 + 5 must be 7", () => {
         const result = sum(2, 5);
 
-        // Criando uma expectativa, onde ela seja verdadeira, no caso do exemplo resultado = 7
         expect(result).toBe(sumResult);
     });
 
