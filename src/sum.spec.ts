47e72458d488d0fdd4ad9172cc011fd7cafa8d57
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-02-22 19:34:07 -0300

    create a test suite

--- src/sum.spec.ts
+++ src/sum.spec.ts
@@ -1,8 +1,25 @@
 import { sum } from "./server";
 
-test("sum of 2 + 5 must be 7", () => {
-    const result = sum(2, 5);
+// Criando um describe para um conjunto de testes
+describe("sum", () => {
+    test("sum of 2 + 5 must be 7", () => {
+        const result = sum(2, 5);
 
-    // Criando uma expectativa, onde ela seja verdadeira, no caso do exemplo resultado = 7
-    expect(result).toBe(7);
+        // Criando uma expectativa, onde ela seja verdadeira, no caso do exemplo resultado = 7
+        expect(result).toBe(7);
+    });
+
+    test("sum of 2 + 2 must be 4", () => {
+        const result = sum(2, 2);
+
+        expect(result).toBe(4);
+    });
+});
+
+describe("sum 2", () => {
+    test("sum of 2 + 2 must be 4", () => {
+        const result = sum(2, 2);
+
+        expect(result).toBe(4);
+    });
 });
