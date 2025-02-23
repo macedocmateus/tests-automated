2a16239cce98e82bc4b57185015fd0c4fdb17631
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-02-23 17:08:05 -0300

    validating test E2E

--- src/products.test.ts
+++ src/products.test.ts
@@ -6,5 +6,14 @@ describe("products", () => {
     it("should list products", async () => {
         const response = await request(app).get("/products");
         console.log(response.body);
+
+        // Criando uma verificação de uma expectativa para o status da requisição
+        expect(response.status).toBe(200);
+
+        // Criando uma verificação de uma expectativa para a quantidade de produtos no corpo da requisição
+        expect(response.body).toHaveLength(3);
+
+        // Criando uma verificação de uma expectativa de um conteúdo retornado
+        expect(response.body.length).toBeGreaterThan(0);
     });
 });
